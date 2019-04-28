7fd20cbd971075a475cf29041a4f4b6dfa778f4b
Jeffrey Warren <jeff@unterbahn.com>
2019-04-28 17:03:59 -0400

    Create webjack-p5-wrapper.js

---
+++ webjack-p5-wrapper.js
@@ -0,0 +1,30 @@
+WebJack = {
+  onReceive: function(handler) {
+    // nonsense to start up audio and get past the "click to play" policy
+    var ac = getAudioContext();
+    ac.suspend().then(function() {
+      var myButton = createButton('click to start audio');
+      myButton.position(0, 0);
+
+      userStartAudio(myButton, function() {
+        myButton.remove();
+
+        // https://github.com/publiclab/webjack/blob/master/src/profiles.js
+        var profile = WebJack.Profiles.SoftModem;
+        var connection = new WebJack.Connection(profile);
+
+        // runs every time a signal is 'heard'
+        connection.listen(function(data) {
+
+          // data from WebJack may look like "1,2,3"
+          data = data.split(',').map(function(i) {
+            return parseFloat(i)
+          }); // so we break it apart using the commas, and make them floats
+
+          handler(data);
+        });
+
+      });
+    });
+  }
+}
